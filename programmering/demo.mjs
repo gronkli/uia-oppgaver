db3d5be4a3763049238b33b9ac7fce11cd5c1696
gronkli <olafwox@hotmail.com>
2024-09-17 13:39:31 +0200

    oppgave delvis ferdig

---
+++ programmering/demo.mjs
@@ -0,0 +1,6 @@
+let firstName = "Olaf"; //tekst /string
+let yearsAsStudent = 4; //tall
+let isStudentNow = true; //boolean true/false
+let studenter = ["Olaf","Alexander","Tony"]
+console.log(studenter[0]); //starter med 0==1
+console.log(firstName);
